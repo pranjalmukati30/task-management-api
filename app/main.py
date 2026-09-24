820a1cbcb91357ffe05e06262d77049a23d3e18e
Pranjal Mukati <pranjalmukati30@gmail.com>
2026-09-24 23:44:51 +0530

    feat: add task CRUD operations

--- app/main.py
+++ app/main.py
@@ -21,6 +21,24 @@ class ProjectUpdate(BaseModel):
     name : str | None = None
     description : str | None = None
 
+
+# Tasks Pydantic model
+class Task(BaseModel):
+    title : str
+    description : str
+    status : str
+    priority : str
+    project_id : int
+    assigned_to : int
+
+class TaskUpdate(BaseModel):
+    title : str | None = None
+    description : str | None = None
+    status : str | None = None
+    priority : str | None = None
+    project_id : int | None = None
+    assigned_to : int | None = None
+
 app = FastAPI()
 
 users = [
@@ -32,6 +50,12 @@ projects = [
     {"id": 1,"name": "Task Management API","description": "Backend API for managing projects and tasks","owner_id": 1},
     {"id": 2,"name": "E-Commerce Backend","description": "Backend system for an online shopping platform","owner_id": 2},
     {"id": 3,"name": "College Management System","description": "System for managing students, courses and faculty","owner_id": 1}
+]
+
+tasks = [{"id": 1,"title": "Design API structure","description": "Plan the endpoints and overall API structure","status": "pending","priority": "high","project_id": 1,"assigned_to": 2},
+    {"id": 2,"title": "Implement User Authentication","description": "Add login and authentication functionality","status": "in_progress","priority": "high","project_id": 1,"assigned_to": 1},
+    {"id": 3,"title": "Create Product APIs","description": "Build APIs for products and categories","status": "pending","priority": "medium","project_id": 2,"assigned_to": 2},
+    {"id": 4,"title": "Create Student Module","description": "Implement student management functionality","status": "completed","priority": "low","project_id": 3,"assigned_to": 1}
 ]
 
 @app.get("/")
@@ -123,3 +147,48 @@ def delete_project(project_id:int):
             projects.pop(index)
             return {"message" : "Project deleted successfully"}
     raise HTTPException(status_code=404, detail=f"Project {project_id} not found")
+
+
+# task endpoints
+@app.get("/tasks",tags=["Tasks"])
+def all_tasks():
+    return tasks
+
+@app.get("/tasks/{task_id}",tags=["Tasks"])
+def task(task_id:int):
+    for task in tasks:
+        if task.get("id") == task_id:
+            return task
+
+    raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
+
+@app.post("/tasks",tags=["Tasks"])
+def create_task(task:Task):
+    new_task = {
+        "id" : len(tasks)+1,
+        "title" : task.title,
+        "description" : task.description,
+        "status" : task.status,
+        "priority" : task.priority,
+        "project_id" : task.project_id,
+        "assigned_to" : task.assigned_to
+    }
+    tasks.append(new_task)
+    return new_task
+
+@app.put("/tasks/{task_id}",tags=["Tasks"])
+def update_task(task:TaskUpdate, task_id:int):
+    for existing_task in tasks:
+        if existing_task["id"] == task_id:
+            updated_task = task.model_dump(exclude_unset=True)
+            existing_task.update(updated_task)
+            return existing_task
+    raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
+
+@app.delete("/tasks/{task_id}",tags=["Tasks"])
+def delete_task(task_id:int):
+    for index,task in enumerate(tasks):
+        if task["id"] == task_id:
+            tasks.pop(index)
+            return {"message" : "Task deleted successfully"}
+    raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
